dee11e13ee08d507f4953a3c659bba927ba71896
szy.majorek@gmail.com <szy.majorek@gmail.com>
2020-03-19 15:13:23 +0100

    normalize features

--- test1.py
+++ test1.py
@@ -3,33 +3,31 @@ import os
 import h5py
 import mahotas
 import cv2 as cv
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from skimage import feature
 
-size = tuple((100, 100))
 
-# shapes
-def fd_hu_moments(image):
+def fd_hu_moments(image):  # shapes
     img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     feat = cv.HuMoments(cv.moments(img)).flatten()
     return feat
 
-# haralick texture
-def fd_haralick(image):
+
+def fd_haralick(image):  # haralick texture
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     haralick = mahotas.features.haralick(gray).mean(axis=0)
     return haralick
 
-# color histogram
-def fd_histogram(image):
+
+def fd_histogram(image):  # color histogram
     img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     hist = cv.calcHist([img], [0, 1, 2], None, [
         8, 8, 8], [0, 256, 0, 256, 0, 256])
     cv.normalize(hist, hist)
     return hist.flatten()
 
-# local features
-def fd_localBinaryPatters(image, numPoints=24, radius=8):
+
+def fd_localBinaryPatters(image, numPoints=24, radius=8):  # local features
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     lbp = feature.local_binary_pattern(
         gray, numPoints, radius, method="uniform")
@@ -43,12 +41,10 @@ def fd_localBinaryPatters(image, numPoints=24, radius=8):
 
 def getGlobalFeatures(file):
     image = cv.imread(file)
-    image = cv.resize(image, size)
     fv_hu_moments = fd_hu_moments(image)
     fv_haralick = fd_haralick(image)
     fv_histogram = fd_histogram(image)
     fv_lbp = fd_localBinaryPatters(image)
-    # don't forget to add feature vector (fv_) to the hstack here
     return np.hstack([fv_hu_moments, fv_haralick, fv_histogram, fv_lbp])
 
 
@@ -75,8 +71,11 @@ targetNames = np.unique(labels)
 le = LabelEncoder()
 target = le.fit_transform(labels)
 
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledFeatures = scaler.fit_transform(globalFeatures)
+
 h5fData = h5py.File(h5Data, "w")
-h5fData.create_dataset("globalFeatures", data=np.array(globalFeatures))
+h5fData.create_dataset("globalFeatures", data=np.array(rescaledFeatures))
 h5fData.close()
 
 h5fLabel = h5py.File(h5Labels, "w")
