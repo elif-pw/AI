4399ba184ead9a53945a7a277165f874bd0220e8
Assyl99 <salaha@student.mini.pw.edu.pl>
2020-05-08 02:17:43 +0200

    Verysimplegui

--- gui.py
+++ gui.py
@@ -1,15 +1,21 @@
 import warnings
+from tkinter.tix import *
 
 import h5py
-import numpy as np
+import matplotlib
+import self as self
 from matplotlib import pyplot
-import pandas as pd
-import seaborn as sns
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from pandas import np
+
+matplotlib.use('TkAgg')
+from tkinter import *
+
 
 warnings.simplefilter("ignore")
 
 
-# give one metric and specify the dataset (default is not normalized)
 def graph(metric, normal=False):
     metric_results = []
 
@@ -21,7 +27,7 @@ def graph(metric, normal=False):
         model_metric = np.array(model_metricString)
         metric_results.append(model_metric)
 
-    fig = pyplot.figure()
+    fig = Figure(figsize=(5, 3), dpi=100)
 
     if normal:
 
@@ -33,13 +39,19 @@ def graph(metric, normal=False):
 
     ax = fig.add_subplot(111)
 
-    pyplot.boxplot(metric_results, vert=False, whis=1.5,
-                   positions=None, widths=None, patch_artist=False)
+    ax.boxplot(metric_results, vert=False, whis=1.5,
+               positions=None, widths=None, patch_artist=False, )
     ax.set_yticklabels(names)
-    pyplot.show()
+    canvas = FigureCanvasTkAgg(fig, master=top)
+    canvas.get_tk_widget().grid(row=0, column=1, padx=20, pady=20, sticky=E)
+    canvas.draw()
 
 
 if __name__ == '__main__':
+    top = Tk()
+    top.title('Classifier comparsion app')
+    top.geometry("700x400")
+    top.resizable(0, 0)
     h5Results = "output/results.h5"
     h5fResults = h5py.File(h5Results, "r")
     h5NonNormalizedResults = "output/resultsNonNormalized.h5"
@@ -47,22 +59,26 @@ if __name__ == '__main__':
     names = ["RFC", "SVC", "KNN", "LR", "LDA", "NB", "CART", "ABC", "QDA"]
 
     metrics = ["_acc", "_err", "_fit_time", "_score_time"]
-    # _acc metric
 
-    graph(metrics[0], normal=True)
-    graph(metrics[0], normal=False)
-    # # _err metric
-    graph(metrics[1], normal=True)
-    graph(metrics[1], normal=False)
-    #
-    # #
-    # # # _fit_time metric for normalized dataset
-    graph(metrics[2], normal=True)
-    graph(metrics[2], normal=False)
+    labelframe = LabelFrame(top)
+    labelframe.grid(row=0, column=0, padx=20, pady=130)
 
-    # # _score_time metric for not normalized dataset
-    graph(metrics[3], normal=True)
-    graph(metrics[3], normal=False)
+    variable = StringVar()
+    variable.set(metrics[0])  # default value
+
+    w = OptionMenu(labelframe, variable, *metrics)
+    w.grid(row=2, column=0)
+
+
+    var1 = BooleanVar()
+    var1.set(False)
+    Checkbutton(labelframe, text="Normalize data", variable=var1).grid(row=3, column=0)
+
+    Button(labelframe, text="graph", command=lambda: graph(variable.get(), normal=var1.get())).grid(row=4, column=0)
+
+    mainloop()
+
+    top.mainloop()
 
     h5fResults.close()
     h5fNonNormalizedResults.close()
